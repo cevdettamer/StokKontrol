8bb698f220de912b99a6fc50f7e558f6d7eafac2
Tamer Guven <ctamerguven@gmail.com>
2020-05-09 15:09:03 +0300

    Proje son.

---
+++ Controllers/SatisController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StokKontrol.Models.Entity;
+namespace StokKontrol.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+
+            return View();
+        }
+
+        public ActionResult YeniSatis(TBLSATISLAR p)
+        {
+            db.TBLSATISLAR.Add(p);
+            db.SaveChanges();
+
+            return View("Index");
+        }
+    }
+}
